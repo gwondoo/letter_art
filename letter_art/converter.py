4f2328978f2d91b5e49781178214cf18fec6a581
gwondoo <6568kdh@naver.com>
2025-05-26 15:36:52 +0900

    버전 0.1.3으로 업데이트 및 ASCII 변환 로직 개선

--- letter_art/converter.py
+++ letter_art/converter.py
@@ -8,6 +8,13 @@ def convert_image_to_ascii(image_path: str, width: int = 100) -> str:
     new_height = int(aspect_ratio * width * 0.5)
     image = image.resize((width, new_height))
     pixels = image.getdata()
-    ascii_str = "".join(ASCII_CHARS[pixel // 25] for pixel in pixels)
-    ascii_img = "\n".join(ascii_str[i:i+width] for i in range(0, len(ascii_str), width))
+    
+    # 픽셀 밝기값을 ASCII 문자 인덱스로 정규화
+    ascii_str = "".join(
+        ASCII_CHARS[min(pixel * len(ASCII_CHARS) // 256, len(ASCII_CHARS) - 1)]
+        for pixel in pixels
+    )
+    ascii_img = "\n".join(
+        ascii_str[i:i+width] for i in range(0, len(ascii_str), width)
+    )
     return ascii_img
